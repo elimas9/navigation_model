ae700955464ea1e369b77521b6845d1a995112e2
Elisa Massi <elisa.massi9@gmail.com>
2022-11-09 23:46:26 +0100

    Removed print from tests

--- test/test_statistics.py
+++ test/test_statistics.py
@@ -32,7 +32,6 @@ class TestResultsDistance(unittest.TestCase):
         d = dist({"k1": 1, "k2": 0})
         npt.assert_allclose(d, 2)
 
-        print(dist({"k1": 3, "k2": 0}))
         npt.assert_allclose(dist.mean(), 3)
         npt.assert_allclose(dist.median(), 3)
         npt.assert_allclose(dist.std(), 1)
